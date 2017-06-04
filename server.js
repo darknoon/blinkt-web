a6e4ac26572d97bf30a0fdf21f821efe3a82dc9f
Andrew Pouliot <andrew@darknoon.com>
2017-06-03 20:34:51 -0700

    Detect whether we're running on a Raspberry Pi and conditionally require blinkt library

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const cssColor = require('color-functions/lib/css-color');
-const Blinkt = require('node-blinkt');
+const isPi = require('detect-rpi');
+const Blinkt = isPi ? require('node-blinkt') : undefined;
+
+const args = process.argv.slice(2);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,17 +14,21 @@ app.get('/color/:color', (req, res) => {
   const {params:{color}} = req;
   const rgba = cssColor(color);
   if (rgba) {
-    const leds = new Blinkt();
-    const {r, g, b, a} = rgba;
-    leds.setup();
-    leds.setAllPixels(r, g, b, a);
-    leds.sendUpdate();
-    res.send(`Set color to <span style='backgroundColor:${color}'>${JSON.stringify(rgba)}</span>`)
+    if (Blinkt) {
+      const leds = new Blinkt();
+      const {r, g, b, a} = rgba;
+      leds.setup();
+      leds.setAllPixels(r, g, b, a);
+      leds.sendUpdate();
+      leds.sendUpdate();
+    }
+    res.send(`Set color to <span style='background-color:${color}'>${color}</span>`)
   } else {
     res.send(`Error parsing color`)
   }
 })
 
-app.listen(3000, function () {
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
